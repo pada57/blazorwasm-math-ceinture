c76513d3601029a8e0b492416acc337ed70c2197
Steven J <steven.jabbour@gmail.com>
2021-11-28 18:12:17 +0100

    missing files

---
+++ Shared/CeintureRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GenerateurCeinture.Shared {
+    public class CeintureRequestModel {
+        [Required(ErrorMessage = "Le nombre de calculs est requis")]
+        public int? NumberOfExpressions { get; set; }
+
+        [Required(ErrorMessage = "Le temps est requis")]
+        public int? Temps { get; set; }
+
+        public bool GenerateAddition { get; set; }
+        public bool GenerateSubstraction { get; set; }
+        public bool GenerateDivision { get; set; }
+        public bool GenerateMultiplication { get; set; }
+    }
+}
